d56e033b84c7f22c97a134729c101c9d5e9567ac
Thea <TheaVanherst@gmail.com>
2023-01-08 16:52:54 +0000

    Default character data amending

--- Data_Libraries/characterDefaultSettings.js
+++ Data_Libraries/characterDefaultSettings.js
@@ -12,58 +12,68 @@ const defaultData = {
         amp: true},
     hat: {
         offset: {
-            // top: 0, left: 0,
-            sequence: [1, 2, 3, 2]}},
+            top: 0, left: 0,
+            sequence: [1, 2, 3, 2],
+            flip: false}},
     weapon: {
         offset: {
-            top: 0, left: 0}},
+            top: 0, left: 0,
+            sequence: [1, 2, 3, 2],
+            flip: false}},
     ring: {
         offset: {
-            top: 0, left: 0},
+            top: 0, left: 0,
+            sequence: [1, 2, 3, 2],
+            flip: false},
         sequence: [1, 2, 3, 4]},
     boots: {
         offset: {
-            top: 0, left: 0}},
+            top: 0, left: 0,
+            sequence: [1, 2, 3, 2],
+            flip: false}},
     charm: {
         offset: {
             top: 0, left: 0,
-            sequence: [1, 2, 3, 2]}},
+            sequence: [1, 2, 3, 2],
+            flip: false}},
     shovel: {
         offset: {
             top: 0, left: 0,
-            sequence: [-1, 0, 1, 0]}},
+            sequence: [-1, 0, 1, 0],
+            flip: false}},
     torch: {
         offset: {
-            // top: 0, left: 0},
-            flip: false
-        },
-        hip: {
-            offset: {
-                // top: 0, left: 0,
-                sequence: [1, 2, 3, 4]
-            }
-        },
-        special: {
-            bool: false,
-            zIndex: 10,
-            resolution: {
-                height: 0, width: 0
-            },
-            displacement: {
-                top: 0, left: 0,
-                sequence: [1, 1, 1, 1]},
+            top: 0, left: 0,
             sequence: [1, 2, 3, 4],
-            transform: {
-                scaleX: 0,
-                scaleY: 0},
-            fileUrl: ""
+            flip: false}
         },
-
-        clothingData: {
-            bool: true,
-            clothing: 1,
-            head: false,
-            floatSequence: true
+    hip: {
+        offset: {
+            top: 0, left: 0,
+            sequence: [1, 2, 3, 4],
+            flip: false
         }
+    },
+    special: {
+        bool: false,
+        zIndex: 10,
+        resolution: {
+            height: 0, width: 0
+        },
+        displacement: {
+            top: 0, left: 0,
+            sequence: [1, 1, 1, 1]},
+        sequence: [1, 2, 3, 4],
+        transform: {
+            scaleX: 0,
+            scaleY: 0},
+        fileUrl: ""
+    },
+
+    clothingData: {
+        bool: true,
+        clothing: 1,
+        head: false,
+        floatSequence: true
     }
 }
