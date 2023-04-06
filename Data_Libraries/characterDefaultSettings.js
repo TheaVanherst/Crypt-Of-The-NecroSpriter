4476079599226878b91085d893338d6a05dcfe1c
Thea <TheaVanherst@gmail.com>
2023-04-06 23:26:29 +0100

    1.3.6_3 reformatted default character setting data

--- Data_Libraries/characterDefaultSettings.js
+++ Data_Libraries/characterDefaultSettings.js
@@ -2,17 +2,18 @@
 const defaultData = {
     settings: {
         resolution: {
-            width: 24,
-            height: 24,
-            rows: 14,
-            columns: 16
+            width:      24,
+            height:     24,
+            rows:       14,
+            columns:    16
         },
         offset: {
-            head: 0,
-            body: 0
+            head:   0,
+            body:   0
         },
         ampMultiplier: 1,
-        amp: true,
+        amp:        true,
+        head:       true,
 
         fileUrl:    "", // don't use spaces!!!
         headExt:    "_heads",
@@ -20,97 +21,97 @@ const defaultData = {
     },
     hat: {
         offset: {
-            top: 0,
-            left: 0,
-            sequence: [1, 2, 3, 2],
-            flip: false
+            top:    0,
+            left:   0,
+            sequence:   [1, 2, 3, 2],
+            flip:   false
         },
-        sequence: [1, 2, 3, 4]
+        sequence:   [1, 2, 3, 4]
     },
     weapon: {
         offset: {
-            top: 0,
-            left: 0,
-            sequence: [1, 2, 3, 2],
-            flip: false
+            top:    0,
+            left:   0,
+            sequence:   [1, 2, 3, 2],
+            flip:   false
         },
-        sequence: [1, 2, 3, 4]
+        sequence:   [1, 2, 3, 4]
     },
     ring: {
         offset: {
-            top: 0,
-            left: 0,
-            sequence: [1, 2, 3, 2],
-            flip: false
+            top:    0,
+            left:   0,
+            sequence:   [1, 2, 3, 2],
+            flip:   false
         },
-        sequence: [1, 2, 3, 4]
+        sequence:   [1, 2, 3, 4]
     },
     boots: {
         offset: {
-            top: 0,
-            left: 0,
-            sequence: [1, 2, 3, 2],
-            flip: false
+            top:    0,
+            left:   0,
+            sequence:   [1, 2, 3, 2],
+            flip:   false
         },
-        sequence: [1, 2, 3, 4]
+        sequence:   [1, 2, 3, 4]
     },
     charm: {
         offset: {
-            top: 0,
-            left: 0,
-            sequence: [1, 2, 3, 2],
-            flip: false
+            top:    0,
+            left:   0,
+            sequence:   [1, 2, 3, 2],
+            flip:   false
         },
-        sequence: [1, 2, 3, 4]
+        sequence:   [1, 2, 3, 4]
     },
     shovel: {
         offset: {
-            top: 0,
-            left: 0,
-            sequence: [-1, 0, 1, 0],
-            flip: false
+            top:    0,
+            left:   0,
+            sequence:   [-1, 0, 1, 0],
+            flip:   false
         },
-        sequence: [1, 2, 3, 4]
+        sequence:   [1, 2, 3, 4]
     },
     torch: {
         offset: {
-            top: 0,
-            left: 0,
-            sequence: [1, 2, 3, 4],
-            flip: false
+            top:    0,
+            left:   0,
+            sequence:   [1, 2, 3, 4],
+            flip:   false
         },
-        sequence: [1, 2, 3, 4]
+        sequence:   [1, 2, 3, 4]
     },
     hip: {
         offset: {
-            top: 0,
-            left: 0,
-            sequence: [1, 2, 3, 4],
-            flip: false
+            top:    0,
+            left:   0,
+            sequence:   [1, 2, 3, 4],
+            flip:   false
         },
-        sequence: [1, 2, 3, 4]
+        sequence:   [1, 2, 3, 4]
     },
     special: {
-        bool: false,
-        zIndex: 10,
+        bool:       false,
+        zIndex:     10,
         resolution: {
             height: 0,
-            width: 0
+            width:  0
         },
         displacement: {
-            top: 0,
-            left: 0,
-            sequence: [1, 1, 1, 1],
-            float: false,
-            fixed: false
+            top:    0,
+            left:   0,
+            sequence:   [1, 1, 1, 1],
+            float:  false,
+            fixed:  false
         },
-        sequence: [1, 2, 3, 4],
+        sequence:   [1, 2, 3, 4],
         transform: {
-            opacity: 1,
-            scaleX: 1,
-            scaleY: 1
+            opacity:    1,
+            scaleX:     1,
+            scaleY:     1
         },
-        fileUrl: ""
+        fileUrl:    "",
     },
     clothingData: {
         0: {
@@ -119,14 +120,30 @@ const defaultData = {
             head:           false,
             floatSequence:  true,
             settings: [
-                {   name: "hat",    bool: true
-                },{ name: "weapon", bool: true
-                },{ name: "ring",   bool: true
-                },{ name: "boots",  bool: true
-                },{ name: "charm",  bool: true
-                },{ name: "shovel", bool: true
-                },{ name: "torch",  bool: true
-                },{ name: "hip",    bool: true
+                {
+                    name: "hat",
+                    bool: true
+                },{
+                    name: "weapon",
+                    bool: true
+                },{
+                    name: "ring",
+                    bool: true
+                },{
+                    name: "boots",
+                    bool: true
+                },{
+                    name: "charm",
+                    bool: true
+                },{
+                    name: "shovel",
+                    bool: true
+                },{
+                    name: "torch",
+                    bool: true
+                },{
+                    name: "hip",
+                    bool: true
             }],
         },
     },
